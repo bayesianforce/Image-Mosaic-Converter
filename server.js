29c313974dd2977ef95cb6f2bdd9b155118178a8
Valerio Bartolini <valerio.bartolini@ext.addisongloballtd.com>
2019-02-09 08:43:29 +0100

    add CI

--- server.js
+++ server.js
@@ -18,24 +18,31 @@ http.createServer((req, res) => {
     const pathname = url.parse(req.url).pathname;
     let m;
     if (pathname == '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(dir + '/dist/index.html').pipe(res);
         return;
     } else if ((m = pathname.match(/^\/color\/([0-9a-fA-F]{6})/))) {
-        res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
-        res.write(util.format(svgTemplate, config.TILE_WIDTH, config.TILE_HEIGHT, m[1]));
+        res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+        res.write(
+            util.format(
+                svgTemplate,
+                config.TILE_WIDTH,
+                config.TILE_HEIGHT,
+                m[1],
+            ),
+        );
         res.end();
         return;
     } else if ((m = pathname.match(/^\/js\//) || pathname.match(/^\//))) {
         const filename = dir + '/dist/' + pathname;
         const stats = fs.existsSync(filename) && fs.statSync(filename);
         if (stats && stats.isFile()) {
-            res.writeHead(200, { 'Content-Type': 'application/javascript' });
+            res.writeHead(200, {'Content-Type': 'application/javascript'});
             fs.createReadStream(filename).pipe(res);
             return;
         }
     }
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.writeHead(404, {'Content-Type': 'text/plain'});
     res.write('404 Not Found\n');
     res.end();
 }).listen(port);
